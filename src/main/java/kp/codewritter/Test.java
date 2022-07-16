63364ef2e7c5b2aa42048c3965d2109dcc17724b
gitkeshava <gitkeshava@gmail.com>
2022-07-16 20:40:52 +0530

    first commit

---
+++ src/main/java/kp/codewritter/Test.java
@@ -0,0 +1,7 @@
+package kp.codewritter;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/kp/codewritter/xdd.java
@@ -0,0 +1,4 @@
+package kp.codewritter;
+
+public class xdd {
+}
