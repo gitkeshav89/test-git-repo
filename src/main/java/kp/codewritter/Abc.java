7fdf7f86fe3b21f0f2a7bf6b53280fb15b7aca4a
gitkeshava <gitkeshava@gmail.com>
2022-07-30 10:49:59 +0530

    updated from master branch

--- src/main/java/kp/codewritter/Abc.java
+++ src/main/java/kp/codewritter/Abc.java
@@ -1,10 +1,13 @@
 package kp.codewritter;
 
 public class Abc {
- private  String name;
- private  Integer age;
- private  String gender;
- //added new field
- private String address;
- //added pull request
+    private String name;
+
+
+    private Integer age;
+    private String gender;
+    //added new field
+    private String address;
+    //added pull request
+    private String x;
 }
--- src/main/java/kp/codewritter/Test.java
+++ src/main/java/kp/codewritter/Test.java
@@ -2,6 +2,7 @@ package kp.codewritter;
 
 public class Test {
     public static void main(String[] args) {
+        int x;
         System.out.println("hello");
         //added new code
         //I need to work now soemthing else
