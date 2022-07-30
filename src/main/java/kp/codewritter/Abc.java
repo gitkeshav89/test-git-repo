538c98c986a0365d4efcaec6d49abd507d228da5
gitkeshav89 <96523238+gitkeshav89@users.noreply.github.com>
2022-07-30 11:05:13 +0530

    Merge major issues (#4)
    * updated Abc file with fields
    
    * updated from master branch
    
    * updated Abc entity class, and main class

--- src/main/java/kp/codewritter/Abc.java
+++ src/main/java/kp/codewritter/Abc.java
@@ -1,10 +1,32 @@
 package kp.codewritter;
 
 public class Abc {
- private  String name;
- private  Integer age;
- private  String gender;
- //added new field
- private String address;
- //added pull request
+    private String name;
+    private Integer age;
+    private String gender;
+    //added new field
+    private String address;
+
+    public Abc(String name, Integer age, String gender, String address) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
